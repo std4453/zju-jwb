b4574c2ae42bf2623a52647807ea8215201fcb05
Harry Zhang <std4453@foxmail.com>
2018-09-26 17:18:07 +0800

    Add error processing

--- src/getin.js
+++ src/getin.js
@@ -7,6 +7,10 @@ const startSession = async (context) => {
         headers: [['Cookie', serializeCookies(context.cookies)]],
     });
     context.cookies = [...context.cookies, ...parseCookies(headers)];
+    if (context.cookies.filter(({ name }) => name === 'ASP.NET_SessionId').length === 0) {
+        throw new Error('Unable to start session with jwbinfosys.zju.edu.cn!');
+    }
+    console.log('Session with jwbinfosys.zju.edu.cn started.');
 };
 
 const followRedirect = async (context, redirect) => {
@@ -14,6 +18,7 @@ const followRedirect = async (context, redirect) => {
         headers: [['Cookie', serializeCookies(context.cookies)]],
     });
     context.cookies = [...context.cookies, ...parseCookies(headers)];
+    console.log(`Redirected to ${redirect}.`);
 };
 
 const getinDefault2 = async (context) => {
@@ -21,15 +26,17 @@ const getinDefault2 = async (context) => {
         headers: [['Cookie', serializeCookies(context.cookies)]],
     });
     context.cookies = [...context.cookies, ...parseCookies(headers)];
+    console.log('Fetched default2.aspx and obtained system access.');
 };
 
 const getin = async (username, password) => {
     const context = { cookies: [] };
     await startSession(context);
-    const { cookies, redirect } = await login(username, password);
+    const { cookies, redirect } = await login('http://jwbinfosys.zju.edu.cn', username, password);
     context.cookies = [...context.cookies, ...cookies];
     await followRedirect(context, redirect);
     await getinDefault2(context);
+    console.log('jwbinfosys.zju.edu.cn ready!');
 
     return context.cookies.filter(({ name }) => name === 'ASP.NET_SessionId');
 };
--- src/login.js
+++ src/login.js
@@ -4,57 +4,68 @@ const cheerio = require('cheerio');
 const { parseCookies, serializeCookies } = require('./utils/cookie');
 const encode = require('./utils/encode');
 
-const startSession = async (context) => {
-    const response = await fetch('https://zjuam.zju.edu.cn/cas/login?service=http://jwbinfosys.zju.edu.cn', {
+const startSession = async (context, service) => {
+    const response = await fetch(`https://zjuam.zju.edu.cn/cas/login?service=${service}`, {
         headers: [['Cookie', serializeCookies(context.cookies)]],
     });
     context.cookies = [...context.cookies, ...parseCookies(response.headers)];
+    if (context.cookies.filter(({ name }) => name === 'JSESSIONID').length === 0) {
+        throw new Error('Unable to start session with zjuam.zju.edu.cn!');
+    }
     console.log('Session with zjuam.zju.edu.cn started.');
 
     const text = await response.text();
     const $ = cheerio.load(text);
     const execution = $('input[name="execution"]').attr('value');
+    if (typeof execution === 'undefined') throw new Error('Unable to obtain "execution" string!');
+    console.log('Obtained "execution" string from fetched page.');
+
     return execution;
 };
 
-const calcRSA = async (context, password) => {
+const calcRSA = async (context) => {
     const response = await fetch('https://zjuam.zju.edu.cn/cas/v2/getPubKey', {
         headers: [['Cookie', serializeCookies(context.cookies)]],
     });
-    const { modulus, exponent } = await response.json();
-    const encoded = encode(password, modulus, exponent);
     context.cookies = [...context.cookies, ...parseCookies(response.headers)];
-    console.log('RSA-ed password calculated.');
+
+    const { modulus, exponent } = await response.json();
+    console.log('Obtained RSA public key from server.');
+
+    const encoded = encode(context.password, modulus, exponent);
+    console.log('Encoded password with RSA public key.');
     return encoded;
 };
 
-const submitCredentials = async (context, username, encoded, execution) => {
+const submitCredentials = async (context, service, encoded, execution) => {
     const params = new URLSearchParams();
-    params.append('username', username);
+    params.append('username', context.username);
     params.append('password', encoded);
     params.append('_eventId', 'submit');
     params.append('authcode', '');
     params.append('execution', execution);
 
-    const { headers, status } = await fetch('https://zjuam.zju.edu.cn/cas/login?service=http://jwbinfosys.zju.edu.cn', {
+    const { headers, status } = await fetch(`https://zjuam.zju.edu.cn/cas/login?service=${service}`, {
         method: 'POST',
         headers: [['Cookie', serializeCookies(context.cookies)]],
         redirect: 'manual',
         body: params,
     });
-    if (status !== 302) throw new Error('Cannot submit credentials!');
     context.cookies = [...context.cookies, ...parseCookies(headers)];
+    if (status !== 302) throw new Error('Submitted credentials rejected!');
     console.log('Logged into zjuam.zju.edu.cn.');
+    
     return headers.get('Location');
 };
 
-const login = async (username, password) => {
-    const context = { cookies: [] };
-    const execution = await startSession(context);
-    const encoded = await calcRSA(context, password);
-    const redirect = await submitCredentials(context, username, encoded, execution);
+const login = async (service, username, password) => {
+    const context = { cookies: [], username, password };
+    const execution = await startSession(service, context);
+    const encoded = await calcRSA(context);
+    const redirect = await submitCredentials(context, service, encoded, execution);
     const cookies = context.cookies.filter(({ name }) => name !== '_pm0' && name !== 'CASPRIVACY' && name !== 'JSESSIONID');
 
+    if (typeof redirect === 'undefined') throw new Error('Unable to obtain redirect link!');
     console.log(`Redirecting to ${redirect}`);
     return { redirect, cookies };
 };
