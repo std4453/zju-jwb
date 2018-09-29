31ef9e62cf9a0c8789ee498f2c8b39d1f74fbc42
Harry Zhang <std4453@foxmail.com>
2018-09-29 11:41:11 +0800

    Fix index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const getin = require('./getin');
+const getin = require('./src/getin');
 const { getSyllabus } = require('./src/syllabus');
 
 function ZjuJwb(username, password) {
@@ -7,14 +7,25 @@ function ZjuJwb(username, password) {
     this.password = password;
 }
 
-ZjuJwb.prototype.login = async function() {
+/**
+ * Login into jwbinfosys.zju.edu.cn system.
+ * @throws If the login process failed with an error.
+ */
+ZjuJwb.prototype.login = async function login() {
     this.session = await getin(this.username, this.password);
-    this.logginIn = true;
-}
+    this.loggedIn = true;
+};
 
-ZjuJwb.prototype.getSyllabus = async function() {
-    if (!this.logginIn) throw new Error("Not logged in!");
-    return await getSyllabus(this.username, this.session);
-}
+/**
+ * Get syllabus of the current school year.
+ * This method should be called only after login() returned with
+ * no error. this.loggedIn can be used to check whether the login
+ * is successful.
+ * @throws If the syllabus cannot be retrieved.
+ */
+ZjuJwb.prototype.getSyllabus = async function getS() {
+    if (!this.loggedIn) throw new Error('Not logged in!');
+    return getSyllabus(this.username, this.session);
+};
 
 module.exports = ZjuJwb;
